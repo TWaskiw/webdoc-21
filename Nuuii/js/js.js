5f2ef8027a453b5c8db8beebee53b64057048bdd
TWaskiw <70579708+TWaskiw@users.noreply.github.com>
2021-03-02 11:40:18 +0100

    Add files via upload

--- Nuuii/js/js.js
+++ Nuuii/js/js.js
@@ -3,3 +3,51 @@ var e = document.getElementById('tree');
 e.onmouseover = function() {
     document.getElementById ('popup1').style.display = 'block';
 }
+
+e.onmouseout = function() {
+    document.getElementById('popup1').style.display = 'none';
+  }   
+
+
+  var e = document.getElementById('vial');
+
+e.onmouseover = function() {
+    document.getElementById ('popup2').style.display = 'block';
+}
+
+e.onmouseout = function() {
+    document.getElementById('popup2').style.display = 'none';
+  }   
+
+
+  var e = document.getElementById('truck');
+
+e.onmouseover = function() {
+    document.getElementById ('popup3').style.display = 'block';
+}
+
+e.onmouseout = function() {
+    document.getElementById('popup3').style.display = 'none';
+  }  
+
+
+  var e = document.getElementById('use');
+
+e.onmouseover = function() {
+    document.getElementById ('popup4').style.display = 'block';
+}
+
+e.onmouseout = function() {
+    document.getElementById('popup4').style.display = 'none';
+  }  
+
+  
+  var e = document.getElementById('recycle');
+
+e.onmouseover = function() {
+    document.getElementById ('popup5').style.display = 'block';
+}
+
+e.onmouseout = function() {
+    document.getElementById('popup5').style.display = 'none';
+  }  
